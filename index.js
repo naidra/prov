25984a401fcf52611bbbebcc5a2163fd4d195cda
ardian sallauka <ardiansallauka@gmail.com>
2017-05-23 16:20:29 +0200

    first2 commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// me Ctrl + c e nal serverin n CMD
+/*// me Ctrl + c e nal serverin n CMD
 const router = require('./router.js');
 const http = require('http');
 
@@ -10,3 +10,18 @@ http.createServer(function(request, response) {
 }).listen(3000);
 
 console.log('Server running at localhost:3000');
+*/
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
