728de3626bbe32cc6d7cb56483163a9c2df93e7d
ardian sallauka <ardiansallauka@gmail.com>
2017-05-23 16:46:29 +0200

    first2 commit

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 /*// me Ctrl + c e nal serverin n CMD
-const router = require('./router.js');
 const http = require('http');
 
-http.createServer(function(request, response) {
-
-	router.home(request, response);
-	router.user(request, response);
-
-}).listen(3000);
 
 console.log('Server running at localhost:3000');
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
 */
+const router = require('./router.js');
 const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+const server = http.createServer(function(request, response) {
+
+	router.home(request, response);
+	router.user(request, response);
+
 });
 
 /*server.listen(port, hostname, () => {
